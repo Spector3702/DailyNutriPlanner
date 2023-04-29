99163df5cded9dd26ec3d723ec867a6d7032f22b
LAPTOP-O5IOA406\Firston.Fu <firston609@gmail.com>
2023-04-29 17:08:02 +0800

    5:07

--- Data/schema.sql
+++ Data/schema.sql
@@ -4,13 +4,12 @@ CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`foodstuff` (
     `Other_name` varchar(60),
     `English_name` varchar(30),
     `Describe` varchar(60),
-    PRIMARY KEY (`Sno.`)
+    PRIMARY KEY (`Sno`)
     UNIQUE (`Name`)
     
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`everyday_nutrition` (
-<<<<<<< HEAD
     `Name` varchar(19),
     `Analysis_item` varchar(12),
     `Unit` varchar(4),
@@ -21,13 +20,14 @@ CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`everyday_nutrition` (
     FOREIGN KEY (`Name`) REFERENCES `foodstuff`(`Name`) 
     ON DELETE SET NULL ON UPDATE CASCADE
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-=======
-    `Name` varchar(20),
-    `Analysis_item` varchar(20),
-    `Unit` varchar(4),
-    `Content_per_unit` float,
-    `Weight_per_unit` varchar(20),
-    `Content_per_unit_weight` float,
-    PRIMARY KEY (`Name`, `Analysis_item`)
+
+CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`recommended_nutrition` (
+    `Sno` char(8),
+    `Name` varchar(19),
+    `Other_name` varchar(60),
+    `English_name` varchar(30),
+    `Describe` varchar(60),
+    PRIMARY KEY (`Sno`)
+    UNIQUE (`Name`)
+    
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
->>>>>>> origin/main
