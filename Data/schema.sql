a4817130614907c05f22fddb34d84be0a47078db
LAPTOP-O5IOA406\Firston.Fu <firston609@gmail.com>
2023-04-29 17:24:28 +0800

    5:24

--- Data/schema.sql
+++ Data/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`foodstuff` (
     `Other_name` varchar(60),
     `English_name` varchar(30),
     `Describe` varchar(60),
-    PRIMARY KEY (`Sno`)
+    PRIMARY KEY (`Sno`),    
     UNIQUE (`Name`)
     
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -15,19 +15,34 @@ CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`everyday_nutrition` (
     `Unit` varchar(4),
     `Content_per_unit` varchar(20),
     `Weight_per_unit` varchar(10),
-    `Content_per_unit_weight` float(24),
+    `Content_per_unit_weight` float,
     PRIMARY KEY (`Name`,`Analysis_item`),
     FOREIGN KEY (`Name`) REFERENCES `foodstuff`(`Name`) 
-    ON DELETE SET NULL ON UPDATE CASCADE
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`recommended_nutrition` (
-    `Sno` char(8),
-    `Name` varchar(19),
-    `Other_name` varchar(60),
-    `English_name` varchar(30),
-    `Describe` varchar(60),
-    PRIMARY KEY (`Sno`)
-    UNIQUE (`Name`)
+    `Gender` varchar(6),
+    `Age` varchar(5),
+    `Numbers_of_people` int,
+    `Calorie` float,
+    `Protein` float,
+    `Fat` float,
+    `Carbohydrate` float,
+    `VitaminB1` float,
+    `VitaminB2` float,
+    `VitaminC` float,
+    `Nicotine` float,
+    `VitaminB6` float,
+    `VitaminA` float,
+    `VitaminE` float,
+    `Ca` float,
+    `P` float,
+    `Fe` float,
+    `Mg` float,
+    `Zn` float,
+    `Na` float,
+    `K` float,
+    PRIMARY KEY (`Gender`,`Age`)
     
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
