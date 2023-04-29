60d270ba624ce940f89c05e9c9d6f0cb1efe39d5
LAPTOP-O5IOA406\Firston.Fu <firston609@gmail.com>
2023-04-29 16:48:03 +0800

    no message

--- Data/schema.sql
+++ Data/schema.sql
@@ -1,7 +1,22 @@
+CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`foodstuff` (
+    `Sno.` char(8),
+    `Name` varchar(19),
+    `Other_name` varchar(60),
+    `English_name` varchar(30),
+    `Describe` varchar(60),
+    PRIMARY KEY (`Sno.`)
+    UNIQUE (`Name`)
+    
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`everyday_nutrition` (
     `Name` varchar(19),
     `Analysis_item` varchar(12),
-    `Content_per_unit` varchar(20),
     `Unit` varchar(4),
-    PRIMARY KEY (`Name`, `Analysis_item`)
+    `Content_per_unit` varchar(20),
+    `Weight_per_unit` varchar(10),
+    `Content_per_unit_weight` float(24),
+    PRIMARY KEY (`Name`,`Analysis_item`),
+    FOREIGN KEY (`Name`) REFERENCES `foodstuff`(`Name`) 
+    ON DELETE SET NULL ON UPDATE CASCADE
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
