ca0922b3310f219aaa5c9e26eea457eca065e716
Spector3702 <overfly3702@gmail.com>
2023-04-29 16:32:24 +0800

    update en_df

--- Data/schema.sql
+++ Data/schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS `DailyNutriPlanner`.`everyday_nutrition` (
-    `Name` varchar(19),
-    `Analysis_item` varchar(12),
-    `Content_per_unit` varchar(20),
+    `Name` varchar(20),
+    `Analysis_item` varchar(20),
     `Unit` varchar(4),
+    `Content_per_unit` float,
+    `Weight_per_unit` varchar(20),
+    `Content_per_unit_weight` float,
     PRIMARY KEY (`Name`, `Analysis_item`)
 ) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
